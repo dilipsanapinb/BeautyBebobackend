147f1793e23c1b3f888e9e7500c2a44f27de2913
dilipsanapinb <dilipinbsanap@gmail.com>
2023-05-07 16:46:46 +0530

    token auth

--- middlewares/authenticate.middleware.js
+++ middlewares/authenticate.middleware.js
@@ -2,7 +2,7 @@ const jwt=require("jsonwebtoken")
 require("dotenv").config();
 
 const authenticate = (req, res, next) => {
-    const token = req.headers.authorization;
+    const token = req.headers.authorization.split(' ')[1];
     try {
         if (token) {
             const decoded = jwt.verify(token, process.env.key, function (err, decoded) {
