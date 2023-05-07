bca265942b8d848d4330719970636ccf3fcef543
dilipsanapinb <dilipinbsanap@gmail.com>
2023-05-07 15:40:43 +0530

    beauty bebo backend updated

--- middlewares/authenticate.middleware.js
+++ middlewares/authenticate.middleware.js
@@ -4,14 +4,16 @@ require("dotenv").config();
 const authenticate=(req,res,next)=>{
     const token=req.headers.authorization;
     if(token){
-        const decoded=jwt.verify(token,process.env.key);
-        if(decoded){
+        const decoded = jwt.verify(token, process.env.key, function (err, decoded) {
+            if(decoded){
             const userID=decoded.userID;
             req.body.userID=userID;
             next()
         }else{
             res.send("PLease Login first")
         }
+        });
+        
     }else{
         res.send("Please Login first")
     }
